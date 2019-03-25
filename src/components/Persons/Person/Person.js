ed9a5a0a682b8b429081f047156d88c1866862bf
Olga Lavrenova <olga.lavrenova@jamaza.com>
2019-03-25 19:01:31 +0300

    added PropTypes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,6 @@
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
 import withClass from '../../../hoc/withClass';
 import classes from './Person.css';
 
@@ -7,12 +9,25 @@ class Person extends Component {
         console.log('[Person.js] rendering...');
         return (
             <Fragment>
-                <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old!</p>
+                <p onClick={this.props.click}>
+                    I am {this.props.name} and I am {this.props.age} years old!
+                </p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
+                <input
+                    type="text"
+                    onChange={this.props.changed}
+                    value={this.props.name}
+                />
             </Fragment>
         )
     }
+}
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
 };
 
 export default withClass(Person, classes.Person);
